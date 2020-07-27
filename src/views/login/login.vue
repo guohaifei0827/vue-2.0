c5d90646217fb4a1150865eefa4ca46e987c43ea
Your Name <you@example.com>
2020-07-27 08:42:57 +0800

    # init

---
+++ src/views/login/login.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="body">
+    <span>{{msg}}</span>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: "login",
+    data() {
+      return {
+        msg: '这是登录页面'
+      }
+    }
+  }
+</script>
+
+<style scoped>
+
+  .body {
+    width: 100%;
+    height: 100%;
+    padding: 0;
+    margin: 0;
+    background-color: aqua;
+  }
+
+  .main-bg {
+    width: 100%;
+    height: 100%;
+    padding: 0;
+    margin: 0;
+    background-color: aqua;
+  }
+</style>
